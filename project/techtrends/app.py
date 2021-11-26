fb854cfbb006642b813bcf5b6881bce19c440c05
Manu Agarwal <manuagarwal38@yahoo.com>
2021-11-26 15:36:38 +0530

    commited docker and app related files

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,12 +2,17 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging,sys
+
+counter = 0
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global counter
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    counter += 1
     return connection
 
 # Function to get a post using its ID
@@ -17,6 +22,8 @@ def get_post(post_id):
                         (post_id,)).fetchone()
     connection.close()
     return post
+
+
 
 # Define the Flask application
 app = Flask(__name__)
@@ -36,13 +43,17 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        app.logger.info('A non-existing article is accessed and a 404 page is returned.')
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        title=post['title']
+        app.logger.info('Article "{}" is retrieved.'.format(title))
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('The "About Us" page is retrieved.')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,11 +71,53 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
+            app.logger.info('Article "{}" is created.'.format(title))
 
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+# define the Healthcheck endpoint for the application
+@app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    return response
+
+# define the Metrics endpoint for the application
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"db_connection_count":counter,"posts":len(posts)}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Metrics request successfull')
+    return response
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+     
+    # Create the formatter to format output
+    formatter = '%(levelname)s - %(name)s - %(asctime)s  - %(message)s'
+
+    # Create handlers for STDOUT and STDERR, set their appropriate levels
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.DEBUG)    
+
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.ERROR)
+    
+
+    dual_handlers = [stdout_handler, stderr_handler]
+    logging.basicConfig(level=logging.DEBUG, format=formatter, handlers=dual_handlers)
+
+    
+    app.run(host='0.0.0.0', port='3111')
